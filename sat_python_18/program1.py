88e9c39e04d44fee552019e3357a769367612314
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-06 20:16:54 +0200

    Add files via upload

---
+++ sat_python_18/program1.py
@@ -0,0 +1,5 @@
+print('-Доллар по 8 !!!')
+
+while True:
+	print('Wzlom')
+	print('...')
---
+++ sat_python_18/program2.py
@@ -0,0 +1,33 @@
+#name=input('Введите имя ')
+#print('Привет '+name+'.')
+
+print(10)
+print(100+10)
+print(10-100)
+print(20/3)
+print(10*10)
+print(2**3)
+
+print((2+2)*2)
+
+print("10+10")
+
+milkyWay = 10
+nuts = 5
+
+print(milkyWay)
+print(milkyWay-nuts)
+
+text = 'Alex'
+text2 = '+200$'
+print('text')
+print('Alex'+'+200$')
+print(text+text2)
+
+
+print('a'*50)
+
+a = 1
+if a == 1:
+	print(' Ты можешь купить кирпич! ')
+
