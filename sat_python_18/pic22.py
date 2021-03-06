7ceec4b97377e7a312876d640ccde167540271d1
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-06 20:08:31 +0200

    Add files via upload

---
+++ sat_python_18/pic22.py
@@ -0,0 +1,13 @@
+import turtle
+
+t=turtle.Pen()
+#-----------------
+
+t.shape('turtle')
+t.left(120)
+t.forward(100)
+t.write('Hello wolrd', align='right', font=('Minecraft Rus Regular', 40))
+
+#-----------------
+turtle.done()
+
---
+++ sat_python_18/program4.py
@@ -0,0 +1,4 @@
+win_player = ['Tesla', 'dom na maldivax', '100$', 'kirpich', 'minecraft']
+
+print(win_player)
+print(win_player[0])
