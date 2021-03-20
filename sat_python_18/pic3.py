c2fa8823265fe57df266d1c230d1c81b2b3cd08c
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-20 20:06:40 +0200

    Add files via upload

--- sat_python_18/pic3.py
+++ sat_python_18/pic3.py
@@ -1,3 +1,13 @@
 import turtle
 
+t=turtle.Pen()
 colors = ['black', 'yellow', 'lime']
+
+t.speed('fastest')
+
+for i in range(1000):
+	t.pencolor(colors[i%3])
+	t.left(36)
+	t.forward(i)
+
+turtle.done()
