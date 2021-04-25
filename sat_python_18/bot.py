e89b0eb7906427e95710176eb72b17a8336da589
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-25 22:39:41 +0300

    Add files via upload

--- sat_python_18/bot.py
+++ sat_python_18/bot.py
@@ -10,37 +10,46 @@ bot = telebot.TeleBot('1779940761:AAGGXR8GXB-ivcZO5BUavreq6VDHedkdmRw') # ток
 # ловит команду /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    # печатает в консоль кто писал
-    print(message.chat.first_name, message.text)
-    
-    # создаем кнопку
-    button = types.ReplyKeyboardMarkup()
-    button.row('🧱', 'какой год?')
-    button.row('по чем гречка?')
-    
-    # отправка сообщений
-    bot.send_message( message.chat.id , "Привет, я гречка-бот!👽", reply_markup = button )
+	# печатает в консоль кто писал
+	# print(message.chat.first_name, message.text)
+	
+	# создаем кнопку
+	button = types.ReplyKeyboardMarkup()
+	button.row('🧱', 'какой год?')
+	button.row('по чем гречка?')
+	
+	# отправка сообщений
+	bot.send_message( message.chat.id , "Привет, я гречка-бот!👽", reply_markup = button )
 #-------------------start-----------------------------------------------
 
 #--------------------text-----------------------------------------------
 # ловит любой текcт от человека
 @bot.message_handler(content_types=['text'])
 def text_message(message):
-    # проверяет текст: если привет то бот отвечает "ну хай"
-    # на все остальное бот отвечает "я тебя не понял"
-    if message.text == 'привет':
-        bot.send_message( message.chat.id, "ну хай")
-    elif message.text == 'по чем гречка?':
-        bot.send_message( message.chat.id, '280 рублей' )
-        bot.send_message( message.chat.id, 'https://rutxt.ru/files/13209/original/ede39c3cb2.JPG' )
-    elif message.text == 'какой год?':
-        
-        bot.send_message( message.chat.id, '2077' )
-    elif message.text == "🧱":
-        bot.send_message(message.chat.id, 'ооо кирпичь! 🧱🧱🧱')
-    else:
-        bot.send_message( message.chat.id, "я тебя не понял")
+	print(message.chat.first_name, message.text)
+	# проверяет текст: если привет то бот отвечает "ну хай"
+	# на все остальное бот отвечает "я тебя не понял"
+	if message.text == 'привет':
+		bot.send_message( message.chat.id, "ну хай")
+	elif message.text == 'по чем гречка?':
+		bot.send_message( message.chat.id, '280 рублей' )
+		bot.send_message( message.chat.id, 'https://rutxt.ru/files/13209/original/ede39c3cb2.JPG' )
+	elif message.text == 'какой год?':
+		bot.send_message( message.chat.id, '2077' )
+	elif message.text == "🧱":
+		bot.send_message(message.chat.id, 'ооо кирпичь! 🧱🧱🧱')
+	else:
+		bot.send_message( message.chat.id, "я тебя не понял")
 #--------------------text-----------------------------------------------
+
+#--------------------sticker--------------------------------------------
+@bot.message_handler(content_types=['sticker'])
+def sticker_message(message):
+    # print(message.sticker.file_id)
+    # отправляет стикер
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBNLtghE8qzaio-BVxxls28M1FF4RCRAACCAADwDZPE29sJgveGptpHwQ ')
+    
+#--------------------sticker--------------------------------------------
 
 # запускаем бота
 print('###bot run...')
